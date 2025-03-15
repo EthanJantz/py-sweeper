8ec81efc44914cd4a5779c5de411524489733498
EthanJantz <21095848+EthanJantz@users.noreply.github.com>
2025-03-15 00:22:28 -0500

    add bad chain_reveal

--- minesweeper.py
+++ minesweeper.py
@@ -19,9 +19,10 @@ class Board:
         self.pct_mines = pct_mines # like 0.10
         self.n_mines = floor(self.board_rows * self.board_cols * self.pct_mines)
         self.real_board = [[' ' for _ in range(self.board_cols)] for _ in range(self.board_rows)]
-        self.player_board = [[' ' for _ in range(self.board_cols)] for _ in range(self.board_rows)]
+        self.player_board = [['?' for _ in range(self.board_cols)] for _ in range(self.board_rows)]
+        self.game_over = False
 
-    def generate_board(self, first_cell: List[List[int]]):
+    def generate_board(self, first_cell: List[int]):
         '''
         Generates the minesweeper board.
 
@@ -33,7 +34,7 @@ class Board:
         while mines > 0:
             for row in range(self.board_rows):
                 for col in range(self.board_cols):
-                    if self.real_board[row][col] != 'X' and self.real_board[first_cell[0]][first_cell[1]]:
+                    if self.real_board[row][col] != 'X' and [row, col] != first_cell:
                         is_mine = random() <= self.pct_mines
                         if is_mine and mines > 0:
                             self.real_board[row][col] = 'X'
@@ -81,21 +82,81 @@ class Board:
         Returns:
             A Board object.
         '''
+        def chain_reveal(cell: List, limit: int):
+            '''
+            Recursively look from cell to cell and reveal if it is blank. 
+
+            Parameters:
+                cell (list): A list containing row, col, and value of a cell.
+                limit (int): The total number of adjacent cells to the given cell.
+
+            Returns:
+                None
+            '''
+            for cell in iterate_adjacent_cells(self.real_board, cell[0], cell[1]):
+                if cell[2] in ' 12345678':
+                    if self.player_board[cell[0]][cell[1]] != '?':
+                        limit -= 1
+                        if limit <= 0: return
+                    else:
+                        self.player_board[cell[0]][cell[1]] = self.real_board[cell[0]][cell[1]]
+                        lim = sum(1 for x in iterate_adjacent_cells(self.real_board, cell[0], cell[1]))
+                        chain_reveal(cell, lim)
+            
         revealed_cell_value = self.real_board[row][col]
         self.player_board[row][col] = revealed_cell_value
         if revealed_cell_value == 'X': 
             print('Game over')
             self.show_board('full')
+            self.game_over = True
         else:
-            if revealed_cell_value == ' ':
-                for cell in iterate_adjacent_cells(self.real_board, row, col):
-                    self.reveal_cell(cell[0], cell[1])
+            if revealed_cell_value in ' 12345678':
+                lim = sum(1 for x in iterate_adjacent_cells(self.real_board, row, col))
+                chain_reveal([row, col], lim)
             self.show_board('player')
 
         return self
 
+class Player:
+
+    def __init__(self, board: Board):
+        self.board = board
+        self.game_over = self.board.game_over
+
+    def get_input(self, message: str = 'Input: '):
+        '''
+        Gets input from player. Input must evaluate to an int.
+
+        Parameters:
+            message (str): The input message. 
+
+        Returns:
+            The input value. 
+        '''
+        value = None
+        while not isinstance(value, int):
+            value = input(message)
+            try:
+                value = int(value)
+            except:
+                continue
+
+        return value
+    
+    def check_game_over(self):
+        '''Checks if the game is over.'''
+        self.game_over = self.board.game_over
+
+
 if __name__ == "__main__":
-    b = Board([10, 10], .17)
-    b.generate_board()
-    b.show_board('player')
+    board = Board([10, 10], .15)
+    player = Player(board)
+    row, col = player.get_input("Input row: "), player.get_input("Input col: ")
+    player.board.generate_board([row, col])
+    player.board.reveal_cell(row, col)
+    while not player.game_over:
+        row, col = player.get_input("Input row: "), player.get_input("Input col: ")
+        player.board.reveal_cell(row, col)
+        player.check_game_over()
+    print("Thanks for playing!")
     
